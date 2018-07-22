65210a1f9ff751ddba1e096435da91ff047e6a01
javinor <javinor@gmail.com>
2018-07-22 15:42:14 +0300

    get release times for a package

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const _ = require("lodash")
+const { execSync } = require('child_process') 
 const fs = require("fs")
 
 const getDepsFromPackageJson = path => {
@@ -7,8 +9,18 @@ const getDepsFromPackageJson = path => {
   return deps
 }
 
+const getReleaseTimes = packageName => {
+  const semverRegex = /^\d+\.\d+\.\d+$/
+  const cmd = `npm view --json ${packageName} time`
+  const viewJson = execSync(cmd, {encoding: "utf8"})
+  const view = JSON.parse(viewJson)
+  const semverOnly = _.pickBy(view, (v, k) => semverRegex.test(k))
+  return semverOnly
+}
+
 module.exports = {
-  getDepsFromPackageJson
+  getDepsFromPackageJson,
+  getReleaseTimes
 }
 
 
