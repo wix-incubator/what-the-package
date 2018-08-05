b2b50dc7c3d332be99e043ed93175dba78514440
Gil Meir <gilme@wix.com>
2018-08-05 13:32:40 +0300

    adding getPackageJsonFromGitAt
    Signed-off-by: Gil Meir <gilme@wix.com>

--- src/data-api.js
+++ src/data-api.js
@@ -1,3 +1,5 @@
+const path = require("path")
+const fs = require("fs")
 const _ = require("lodash")
 const util = require("util")
 const exec = util.promisify(require("child_process").exec)
@@ -13,6 +15,31 @@ const getVersionToReleaseTime = async npmModuleName => {
   } else {
     return _.isEmpty(stdout) ? {} : JSON.parse(stdout)
   }
+}
+
+const getPackageJsonFromGitAt = async (timestamp, gitDir) => {
+  const gitDirPath = path.resolve(gitDir, ".git")
+  const pjPath = path.resolve(gitDir, "package.json")
+
+  const errors = []
+  if (!fs.existsSync(gitDirPath)) {
+    errors.push(`Couldn't find a git repository at ${gitDir}`)
+  }
+  if (!fs.existsSync(pjPath)) {
+    errors.push(`Couldn't find package.json at ${pjPath}`)
+  }
+  if (errors.length > 0) {
+    throw new Error(errors.join(", "))
+  }
+
+  const cmd = `cd ${gitDir} && lastCommitBeforeTimestamp=\`git log --format=format:%H --before="${timestamp}" -1\` && echo \`git show \$\{lastCommitBeforeTimestamp\}:./package.json\``
+  const { stdout, stderr } = await exec(cmd, { encoding: "utf8" })
+  if (stderr) {
+    throw new Error(
+      `Failed finding a package.json file on ${timestamp} in the repo at ${gitDir}`
+    )
+  }
+  return Promise.resolve(JSON.parse(stdout))
 }
 
 const getRegistryInfoField = fieldName => async (npmModuleName, timestamp) => {
@@ -36,5 +63,6 @@ const getReleaseTimes = getVersionToReleaseTime
 module.exports = {
   getDependencySemvers,
   getDevDependencySemvers,
-  getReleaseTimes
+  getReleaseTimes,
+  getPackageJsonFromGitAt
 }
--- test/data-api.test.js
+++ test/data-api.test.js
@@ -1,11 +1,15 @@
+const path = require("path")
 const _ = require("lodash")
 const {
   getDependencySemvers,
   getDevDependencySemvers,
-  getReleaseTimes
+  getReleaseTimes,
+  getPackageJsonFromGitAt,
 } = require("../src/data-api")
 
 const detoxRegistryInfo = require("../data/npm-view-detox-8.json")
+
+const mockProjectPastPackageJson = require("../data/mock-project-at-2018-08-05_01-30-21-package")
 
 const {
   dependencies: DETOX_DEPENDENCIES,
@@ -58,5 +62,28 @@ describe("data-api", () => {
   test("should return error message for non-existing package", async () => {
     const actual = getReleaseTimes(NON_EXISTING_PACKAGE_NAME)
     return expect(actual).rejects.toThrow()
+  })
+})
+
+describe("getPackageJsonFromGitAt", () => {
+  test("should return package.json content", async () => {
+    const mockProjectGitDirPath = path.resolve(__dirname, "../data/mock-project")
+    const timestamp = new Date("Sun Aug 5 01:30:21 2018 +0300").valueOf() + 1
+
+    const pj = await getPackageJsonFromGitAt(timestamp, mockProjectGitDirPath)
+
+    expect(pj).toEqual(mockProjectPastPackageJson)
+  })
+
+  test.skip("should throw if given gitDir doesn't exist", () => {
+    expect(true).toEqual(false)
+  })
+
+  test.skip("should throw if couldn't find package.json in given gitDir", () => {
+    expect(true).toEqual(false)
+  })
+
+  test.skip("should throw if there are not commits before given timestamp", () => {
+    expect(true).toEqual(false)
   })
 })
