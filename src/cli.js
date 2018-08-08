6485260ce8bcd2fe0f6eb0b14b53c1223cc9d600
nickg <nickg@wix.com>
2018-08-08 13:37:49 +0300

    Added spinner, refactored cli

--- src/cli.js
+++ src/cli.js
@@ -1,121 +1,9 @@
 #!/usr/bin/env node
 /* eslint no-console: 0 */
-const _ = require("lodash")
-const dayjs = require("dayjs")
-const SV = require("semver")
-const table = require("text-table")
 const createDependencyComparator = require("../src/createDependencyComparator")
 const dataApi = require("../src/data-api")
 const { compareNpmModuleDependencies } = createDependencyComparator(dataApi)
-
-const getConsoleColorConfigurator = isColorDisabled => color => {
-  const consoleColors = {
-    black: "\x1b[30m",
-    red: "\x1b[31m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    blue: "\x1b[34m",
-    magenta: "\x1b[35m",
-    cyan: "\x1b[36m",
-    white: "\x1b[37m",
-    RESET: "\x1b[0m"
-  }
-
-  return !isColorDisabled ? consoleColors[color] : ""
-}
-
-const printSummary = (modulesComparisonList, isColorDisabled) => {
-  const setConsoleColor = getConsoleColorConfigurator(isColorDisabled)
-
-  const {
-    added: addedModules,
-    removed: removedModules,
-    present: presentModules
-  } = _.groupBy(modulesComparisonList, module => {
-    if (_.isNull(module.priorVersion)) {
-      return "added"
-    } else if (_.isNull(module.latterVersion)) {
-      return "removed"
-    } else {
-      return "present"
-    }
-  })
-
-  const changedModules = _.filter(
-    presentModules,
-    module => module.priorVersion !== module.latterVersion
-  )
-
-  const getSemVerType = module => {
-    const svp = SV.coerce(module.priorVersion)
-    const svl = SV.coerce(module.latterVersion)
-
-    if (svp.major !== svl.major) {
-      return "major"
-    } else if (svp.minor !== svl.minor) {
-      return "minor"
-    } else if (svp.patch !== svl.patch) {
-      return "patch"
-    } else {
-      return "other"
-    }
-  }
-
-  console.log("=== Summary ===")
-  console.log("")
-  addedModules ? console.log("Added") : null
-  console.log(
-    table(
-      _.map(addedModules, module => {
-        return [
-          setConsoleColor("green"),
-          module.packageName,
-          setConsoleColor("RESET")
-        ]
-      })
-    )
-  )
-  console.log("")
-  changedModules ? console.log("Changed") : null
-  console.log(
-    table(
-      _.orderBy(
-        _.map(changedModules, module => {
-          return [
-            setConsoleColor("yellow"),
-            module.packageName,
-            module.priorVersion,
-            setConsoleColor("RESET"),
-            "→",
-            setConsoleColor("yellow"),
-            module.latterVersion,
-            setConsoleColor("RESET"),
-            getSemVerType(module)
-          ]
-        }),
-        item => {
-          return item[8]
-        }
-      )
-    )
-  )
-  console.log("")
-  removedModules ? console.log("Removed") : null
-  console.log(
-    table(
-      _.map(removedModules, module => {
-        return [
-          setConsoleColor("red"),
-          module.packageName,
-          setConsoleColor("RESET")
-        ]
-      })
-    )
-  )
-  console.log("")
-  console.log("")
-  console.log("Use the '--raw' flag to get a raw json")
-}
+const ui = require("./cli/ui")
 
 require("yargs")
   .usage(
@@ -133,12 +21,9 @@ require("yargs")
           .then(res => console.log(res))
           .catch(err => console.error(err))
       } else {
-        console.log("")
-        console.log(` * source: ${source}`)
-        console.log(` * prior: ${priorDate}`)
-        console.log(` * latter: ${latterDate}`)
-        console.log("")
-        console.log("=== Comparing Dependencies... ===")
+        ui.printHeader({ source, priorDate, latterDate })
+
+        const spinner = ui.spinner.start()
 
         compareNpmModuleDependencies(
           source,
@@ -146,9 +31,13 @@ require("yargs")
           latterDate.valueOf()
         )
           .then(res => {
-            printSummary(res, noColor)
+            ui.spinner.stop(spinner)
+            ui.printSummary(res, noColor)
           })
-          .catch(err => console.error("Something went wrong:\n", err))
+          .catch(err => {
+            ui.spinner.stop(spinner)
+            console.error("Something went wrong:\n", err)
+          })
       }
     }
   )
---
+++ src/cli/dependecyViews.js
@@ -0,0 +1,83 @@
+/* eslint no-console: 0 */
+
+const SV = require("semver")
+const table = require("text-table")
+const _ = require("lodash")
+const colorsService = require("./colorsService")
+
+const getSemVerType = module => {
+  const svp = SV.coerce(module.priorVersion)
+  const svl = SV.coerce(module.latterVersion)
+
+  if (svp.major !== svl.major) {
+    return "major"
+  } else if (svp.minor !== svl.minor) {
+    return "minor"
+  } else if (svp.patch !== svl.patch) {
+    return "patch"
+  } else {
+    return "other"
+  }
+}
+
+const printAddedModules = (modules, setColor) => {
+  modules ? console.log("Added") : null
+  console.log(
+    table(
+      _.map(modules, module => {
+        return [
+          setColor(colorsService.colors.green),
+          module.packageName,
+          setColor(colorsService.colors.RESET)
+        ]
+      })
+    )
+  )
+}
+
+const printChangedModules = (modules, setColor) => {
+  modules ? console.log("Changed") : null
+  console.log(
+    table(
+      _.orderBy(
+        _.map(modules, module => {
+          return [
+            setColor(colorsService.colors.yellow),
+            module.packageName,
+            module.priorVersion,
+            setColor(colorsService.colors.RESET),
+            "→",
+            setColor(colorsService.colors.yellow),
+            module.latterVersion,
+            setColor(colorsService.colors.RESET),
+            getSemVerType(module)
+          ]
+        }),
+        item => {
+          return item[8]
+        }
+      )
+    )
+  )
+}
+
+const printRemovedModules = (modules, setColor) => {
+  modules ? console.log("Removed") : null
+  console.log(
+    table(
+      _.map(modules, module => {
+        return [
+          setColor(colorsService.colors.red),
+          module.packageName,
+          setColor(colorsService.colors.RESET)
+        ]
+      })
+    )
+  )
+}
+
+module.exports = {
+  printAddedModules,
+  printChangedModules,
+  printRemovedModules
+}
